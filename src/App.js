2eb7bcd95fe62b66ce8ef6e8c92b51b1664c79d0
timka <git@timka.su>
2023-11-23 18:42:02 +0300

    change main component to class

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import AddLink from "./components/AddLink";
 import Redirecter from './components/Redirecter'
 import NotFound from './components/NotFound'
-import {React} from "react";
+import {Component, React} from "react";
 
-function App() {
-  return (
-      <div className="App">
-          <h3 className="title"><a href="/" className="href">Timka ShortLink</a></h3>
-          <BrowserRouter>
-              <Routes>
-                  <Route path="add" element={<AddLink/>} />
-                  <Route path="/" element={<AddLink/>} />
-                  <Route path="404" element={<NotFound/>} />
-                  <Route path="*" element={<Redirecter/>} />
-              </Routes>
-          </BrowserRouter>
-      </div>
-  );
+class App extends Component {
+    render () {
+        return <div className="App">
+            <h3 className="title"><a href="/" className="href">Timka ShortLink</a></h3>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="add" element={<AddLink/>} />
+                    <Route path="/" element={<AddLink/>} />
+                    <Route path="404" element={<NotFound/>} />
+                    <Route path="*" element={<Redirecter/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    }
 }
 
 export default App;
